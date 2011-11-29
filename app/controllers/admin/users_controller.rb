790ad302a75dab6157cde18bd03c6975dc1c9d38
Hector Veiga <kecok0@gmail.com>
2011-11-29 10:10:40 +0100

    Added features for editing users and deleting users, including protection against self-deletion

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < Admin::BaseController
+  before_filter :find_user, :only => [:show, :edit, :update, :destroy]
   
   def index
     @users = User.all(:order => "email")
@@ -10,7 +11,7 @@ class Admin::UsersController < Admin::BaseController
   
   def create
     @user = User.new(params[:user])
-    @user.admin = params[:user][:admin] == "1"
+    set_admin
     if @user.save
       flash[:notice] = "User has been created."
       redirect_to admin_users_path
@@ -19,5 +20,46 @@ class Admin::UsersController < Admin::BaseController
       render :action => "new"
     end
   end
+  
+  def show
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+    end
+    set_admin
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "User has been updated."
+      redirect_to admin_users_path
+    else
+      flash[:alert] = "User has not been updated."
+      render :action => "edit"
+    end
+  end
+  
+  def destroy
+    if @user == current_user
+      flash[:alert] = "You cannot delete yourself!"
+    else
+      @user.destroy
+      flash[:notice] = "User has been deleted."
+    end
+    redirect_to admin_users_path
+  end
+  
+  private
+    def find_user
+      @user = User.find(params[:id])
+    end
+    
+  private
+    def set_admin
+      @user.admin = params[:user][:admin] == "1"
+    end
 
 end
