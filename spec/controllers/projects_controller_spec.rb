0a928ca0c432c054831dbad1aa469cf10c657955
Hector Veiga <kecok0@gmail.com>
2011-11-30 04:17:32 +0100

    Made project only visible to users with permission to see them

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -15,16 +15,24 @@ context "standard users" do
     "edit" => "get",
     "update" => "put",
     "destroy" => "delete" }.each do |action, method|
-    it "cannot access the #{action} action" do
-      sign_in(:user, user)
-      send(method, action.dup, :id => project.id)
-      response.should redirect_to(root_path)
-      flash[:alert].should eql("You must be an admin to do that.")
-    end
-    end
+  it "cannot access the #{action} action" do
+    sign_in(:user, user)
+    send(method, action.dup, :id => project.id)
+    response.should redirect_to(root_path)
+    flash[:alert].should eql("You must be an admin to do that.")
+  end
+  end
+    
+  it "cannot access the show action" do
+    sign_in(:user, user)
+    get :show, :id => project.id
+    response.should redirect_to(projects_path)
+    flash[:alert].should eql("The project you were looking for could not be found.")
+  end
 end
   
 it "displays an error for a missing project" do
+  sign_in(:user, user)
   get :show, :id => "not-here"
   response.should redirect_to(projects_path)
   message = "The project you were looking for could not be found."
